12bc1ae6bc9e47967cbac9c58597c2f863a80be5
SmoothGoL <151787406+SmoothGoL@users.noreply.github.com>
2026-09-21 18:34:07 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SOURCE="./public/"
-DEST="/opt/homebrew/var/www/mobile-ordering-app/"
-
-echo "Syncing changes..."
-rsync -avc --delete "$SOURCE" "$DEST"
-echo "Deployment complete!"
