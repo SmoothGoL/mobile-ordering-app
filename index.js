1daf04a1dd5fb1545615d272e5a32589f25803c7
SmoothGoL <151787406+SmoothGoL@users.noreply.github.com>
2026-09-22 13:37:57 +0200

    Add order HTML

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 import { menuArray } from './data.js';
 
-const mainEl = document.querySelector('main');
+const menuItemsEl = document.querySelector('.menu-items');
+const orderEl = document.querySelector('.order');
+const myOrder = [0, 2];
 
 function getMenuHtml(menuItems) {
     let outputHtml = '';
     menuItems.forEach((menuItem) => {
         outputHtml += `
-            <section>
+            <section class="menu-item">
                 <span class="dish-icon">${menuItem.emoji}</span>
-                <div>
-                    <h2 class="menu-item-title">${menuItem.name}</h2>
+                <div class="menu-item-description">
+                    <h3 class="menu-item-title">${menuItem.name}</h3>
                     <p class="ingredients">${menuItem.ingredients.join(',')}</p>
                     <p class="price">$${menuItem.price}</p>
                 </div>
+                <button class="add-btn"><span class="add-btn-text">+</span></button>
             </section>
         `;
     });
     return outputHtml;
 }
 
-mainEl.innerHTML = getMenuHtml(menuArray);
+function getOrderHtml(orderArr) {
+    if (orderArr.length > 0) {
+        let outputHtml = '<h2 class="order-title">Your order</h2>';
+        let totalPrice = 0;
+        orderArr.forEach((orderId, index) => {
+            const orderItem = menuArray.filter(menuItem => menuItem.id === orderId)[0];
+            outputHtml += `<div class="order-item-container">
+                <span class="order-item">${orderItem.name}</span>
+                <button class="remove-btn" data-remove-order-index="${index}">remove</button>
+                <span class="order-price">$${orderItem.price}</span>
+            </div>`;
+            totalPrice += orderItem.price;
+        });
+
+        outputHtml += `
+            <hr class="order-hr">
+            <div class="total-price-container">
+                <span class="total-price">Total price:</span>
+                <span class="order-price">$${totalPrice}</span>
+            </div>
+            <button class="complete-order-btn">Complete order</button>`;
+        return outputHtml;
+    } else {
+        return '';
+    }
+}
+
+function renderOrder(orderArr) {
+    orderEl.innerHTML = getOrderHtml(orderArr);
+}
+
+menuItemsEl.innerHTML = getMenuHtml(menuArray);
+renderOrder(myOrder);
